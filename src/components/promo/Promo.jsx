bafc63021366127735984d82e92659f4764125ef
GreedIsGoody <norman.tsar@gmail.com>
2026-01-14 20:43:36 +0200

    First commit of fashion shopping page

---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,13 @@
+import './promo.css'
+const Promo = () => {
+    return ( <section className="promo">
+        <div className="container">
+            <div className="promo">
+                <div className="promo_text">TEXT</div>
+                <div className="promo_img">IMG</div>
+            </div>
+        </div>
+    </section> );
+}
+ 
+export default Promo;
